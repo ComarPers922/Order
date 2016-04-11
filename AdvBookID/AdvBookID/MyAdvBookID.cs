d3208eabb1e29ff530657a56a0538c58fcd19505
ComarPers922 <18982127626@163.com>
2016-04-11 22:52:53 +0800

    最终版本

---
+++ AdvBookID/AdvBookID/MyAdvBookID.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using BookID;
+
+namespace AdvBookID
+{
+    public class MyAdvBookID:IComparable<MyAdvBookID>,IComparer<MyAdvBookID>
+    {
+        public string TheText = "";
+
+        public MyAdvBookID(string text)
+        {
+            TheText = text;
+        }
+        public int Compare(MyAdvBookID x, MyAdvBookID y)
+        {
+            return x.CompareTo(y);
+        }
+        public int CompareTo(MyAdvBookID other)
+        {
+            int returnValue = 0;
+            string thisText = TheText.Split('/')[0];
+            string otherText = other.TheText.Split('/')[0];
+
+            try
+            {
+                for (int i = 0; i < thisText.Length; i++)
+                {
+                    if (thisText[i] > otherText[i])
+                    {
+                        returnValue = -1;
+                        break;
+                    }
+                    else if (thisText[i] < otherText[i])
+                    {
+                        returnValue = 1;
+                        break;
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                returnValue = -1;
+            }
+            returnValue = (returnValue == 0 ? (new MyBookID(TheText)).CompareTo(new MyBookID(other.TheText)) : returnValue);
+            return returnValue;
+        }
+
+        public override string ToString()
+        {
+            return TheText;
+        }
+    }
+}
---
+++ BookID/BookID/BookID.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static System.Convert;
+
+namespace BookID
+{
+    public class MyBookID : IComparable<MyBookID>,IComparer<MyBookID>
+    {
+        public string ID { set; get; }
+        public MyBookID(string id)
+        {
+            ID = id;
+        }
+
+        public int CompareTo(MyBookID other)
+        {
+            int i = 0;
+            string thisText = ID.Replace('-', '?'); ;
+            string otherText = other.ID.Replace('-', '?'); ;
+            int returnValue = 0;
+            int j = 0;
+            if (char.IsDigit(thisText[0]) && !char.IsDigit(otherText[0]))
+            {
+                returnValue = 1;
+                goto result;
+            }
+            if (!char.IsDigit(thisText[0]) && char.IsDigit(otherText[0]))
+            {
+                returnValue = -1;
+                goto result;
+            }
+            try
+            {
+                bool fraction = false;
+                int div = 10;
+                for (i = 0; i < thisText.Length;)
+                {
+                    if (char.IsDigit(thisText[i]) && !char.IsDigit(otherText[i]))
+                    {
+                        returnValue = otherText[i] == '/' ? -1 : 1;
+                        goto result;
+                    }
+                    if (!char.IsDigit(thisText[i]) && char.IsDigit(otherText[i]))
+                    {
+                        returnValue = thisText[i] == '/' ? 1 : -1;
+                        goto result;
+                    }
+                    double x = 0, y = 0;
+                    try
+                    {
+                        for (j = i; j < thisText.Length; j++)
+                        {
+                            if (char.IsDigit(thisText[j]))
+                            {
+                                if (!fraction)
+                                {
+                                    x *= 10;
+                                    x += ToInt32(thisText[j].ToString());
+                                }
+                                else
+                                {
+                                    x += ToDouble(thisText[j].ToString()) / div;
+                                    div *= 10;
+                                }
+                            }
+                            else if (thisText[j] == '.')
+                            {
+                                fraction = true;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        div = 10;
+                        fraction = false;
+                        for (int k = i; k < otherText.Length; k++)
+                        {
+                            if (char.IsDigit(otherText[k]))
+                            {
+                                if (!fraction)
+                                {
+                                    y *= 10;
+                                    y += ToInt32(otherText[k].ToString());
+                                }
+                                else
+                                {
+                                    y += ToDouble(otherText[k].ToString()) / div;
+                                    div *= 10;
+                                }
+                            }
+                            else if (otherText[k] == '.')
+                            {
+                                fraction = true;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        fraction = false;
+                        div = 10;
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        if (x > y)
+                        {
+                            returnValue = -1;
+                            goto result;
+                        }
+                        if (x < y)
+                        {
+                            returnValue = 1;
+                            goto result;
+                        }
+                        else
+                        {
+                            returnValue = 0;
+                        }
+                    }
+
+                    if (x > y)
+                    {
+                        returnValue = -1;
+                        goto result;
+                    }
+                    if (x < y)
+                    {
+                        returnValue = 1;
+                        goto result;
+                    }
+                    else
+                    {
+                        returnValue = 0;
+                    }
+                    i += j - i;
+
+                    if (thisText[i] == '-' && otherText[i] != '-')
+                    {
+                        returnValue = -1;
+                        goto result;
+                    }
+                    else if (thisText[i] != '-' && otherText[i] == '-')
+                    {
+                        returnValue = 1;
+                        goto result;
+                    }
+
+                    if (thisText[i] > otherText[i])
+                    {
+                        returnValue = -1;
+                        goto result;
+                    }
+                    else if (thisText[i] < otherText[i])
+                    {
+                        returnValue = 1;
+                        goto result;
+                    }
+                    else
+                    {
+                        returnValue = 0;
+                        i++;
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                if (thisText.Length > otherText.Length)
+                {
+                    returnValue = -1;
+                }
+                if (thisText.Length < otherText.Length)
+                {
+                    returnValue = 1;
+                }
+            }
+        result:
+            return returnValue;
+        }
+
+        public override string ToString()
+        {
+            return ID;
+        }
+
+        public int Compare(MyBookID x, MyBookID y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+}
